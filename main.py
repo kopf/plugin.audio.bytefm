7db62422190dcc24e15cae48e23e101843741b1a
Aengus Walton <ventolin@gmail.com>
2017-03-04 13:28:50 +0100

    Add cuefile stuff

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 
 import requests
-
 from simpleplugin import Plugin
 
 
@@ -15,7 +14,19 @@ LETTERS = '0ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 PLUGIN_NAME = 'plugin.audio.bytefm'
 
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+
 AUTH = ('lol', 'lol')
+
+CUE_TEMPLATE = u'''PERFORMER "{moderators}"
+TITLE "{broadcast_title}"
+'''
+CUE_ENTRY_TEMPLATE = u'''FILE "{filename}" MP3
+  TRACK {tracknumber} AUDIO
+    TITLE "{title}"
+    PERFORMER "{artist}"
+    INDEX 01 {timestamp}
+'''
 
 
 def _http_get(url, auth=None):
@@ -51,6 +62,17 @@ def _get_subtitle(broadcast):
     else:
         return u'Broadcast from {}'.format(broadcast['date'])
 
+def _save_cuefile(playlist, cue_path, mp3_path, moderators, broadcast_title):
+    with open(cue_path, 'w') as f:
+        f.write(CUE_TEMPLATE.format(
+            moderators=moderators, broadcast_title=broadcast_title).encode('utf-8'))
+        for idx, entry in enumerate(playlist):
+            minutes, seconds = divmod(entry['time'], 60)
+            timestamp = "%02d:%02d:00" % (minutes, seconds)
+            f.write(CUE_ENTRY_TEMPLATE.format(
+                filename=mp3_path, tracknumber='%02d' % idx, title=entry['title'],
+                artist=entry['artist'], timestamp=timestamp).encode('utf-8'))
+
 
 @plugin.action()
 def root(params):
@@ -58,6 +80,7 @@ def root(params):
         {
             'label': 'Listen live',
             'url': 'http://byte.fm/livestream.mp3',
+            'icon': os.path.join(THIS_DIR, 'icon.png'),
             'is_playable': True
         },
         {
@@ -96,7 +119,9 @@ def list_shows(params):
                 show_img = _get_img_url(show)
                 items.append({
                     'label': show['title'],
-                    'url': plugin.get_url(action='list_broadcasts', slug=show['slug'], show_img=show_img),
+                    'url': plugin.get_url(
+                        action='list_broadcasts', slug=show['slug'], show_img=show_img,
+                        moderators=show['moderators'], show_title=show['title']),
                     'thumbnail': _get_img_url(show),
                     'icon': _get_img_url(show)
                 })
@@ -106,7 +131,9 @@ def list_shows(params):
                 show_img = _get_img_url(show)
                 items.append({
                     'label': show['title'],
-                    'url': plugin.get_url(action='list_broadcasts', slug=show['slug'], show_img=show_img),
+                    'url': plugin.get_url(
+                        action='list_broadcasts', slug=show['slug'], show_img=show_img,
+                        moderators=show['moderators'], show_title=show['title']),
                     'thumbnail': _get_img_url(show),
                     'icon': _get_img_url(show)
                 })
@@ -122,10 +149,12 @@ def list_broadcasts(params):
     return [
         {
             'label': _get_subtitle(broadcast),
-            'url': plugin.get_url(action='play', show_slug=params['slug'], broadcast_date=broadcast['date']),
+            'url': plugin.get_url(
+                action='play', show_slug=params['slug'], broadcast_date=broadcast['date'],
+                moderators=params['moderators'], title=_get_subtitle(broadcast)),
             'icon': _get_img_url(broadcast) or params['show_img'],
             'thumbnail': _get_img_url(broadcast) or params['show_img'],
-            'info': {'video': {'plot': broadcast['description']}},
+            #'info': {'video': {'plot': broadcast['description']}},
             'is_playable': True
         } for broadcast in _get_broadcasts(params['slug'])
     ]
@@ -135,16 +164,20 @@ def list_broadcasts(params):
 def play(params):
     broadcast_data = _get_broadcast_url_playlist(params['show_slug'], params['broadcast_date'])
     url = broadcast_data['url']
-    playlist = broadcast_data['playlist']
+    playlist = reversed(broadcast_data['playlist'])
     mp3_filename = url.replace('/', '_').replace(' ', '_')
     mp3_path = os.path.join(plugin.config_dir, mp3_filename)
+    cue_path = mp3_path + '.cue'
     resp = requests.get('http://archiv.byte.fm' + url, stream=True, auth=AUTH)
     # TODO: Assert http 200
     if not os.path.isfile(mp3_path):
+        plugin.log_error('{} does not exist'.format(mp3_path))
         with open(mp3_path, 'wb') as f:
             for block in resp.iter_content(4096):
                 f.write(block)
-    return 'special://profile/addon_data/{}/{}'.format(PLUGIN_NAME, mp3_filename)
+    if not os.path.isfile(cue_path):
+        _save_cuefile(playlist, cue_path, mp3_path, params['moderators'], params['title'])
+    return 'special://profile/addon_data/{}/{}'.format(PLUGIN_NAME, mp3_filename + '.cue')
 
 
 

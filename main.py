694097f603bd401a8db6fe2dbf457dc4a13cb512
Aengus Walton <ventolin@gmail.com>
2017-03-04 16:36:25 +0100

    Show progress bar when downloading

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 
 import requests
 from simpleplugin import Plugin
+import xbmcgui
 
 
 plugin = Plugin()
@@ -15,6 +16,8 @@ ARCHIVE_BASE_URL = 'http://archiv.byte.fm'
 LETTERS = '0ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 PLUGIN_NAME = 'plugin.audio.bytefm'
+
+CHUNK_SIZE = 1024 * 1024 # 1MB
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -152,14 +155,17 @@ def list_shows(params):
             'label': show['title'],
             'url': plugin.get_url(
                 action='list_broadcasts', slug=show['slug'], show_img=show_img,
-                moderators=show['moderators'], show_title=show['title']),
+                moderators=show['moderators'] or 'Unknown', show_title=show['title']),
             'thumbnail': _get_img_url(show),
             'icon': _get_img_url(show)
         }
 
     if params.get('letter'):
+        letter = params['letter'].lower()
+        if letter == '0':
+            letter = '0123456789'
         for show in shows:
-            if show['title'].lower().startswith(params['letter'].lower()):
+            if show['title'].lower()[0] in letter:
                 items.append(_create_show_listitem(show))
     elif params.get('genre'):
         for show in shows:
@@ -201,16 +207,24 @@ def play(params):
     broadcast_data = _get_broadcast_url_playlist(params['show_slug'], params['broadcast_date'])
     url = broadcast_data['url']
     playlist = reversed(broadcast_data['playlist'])
-    mp3_filename = url.replace('/', '_').replace(' ', '_')
+    mp3_filename = url.replace('/', '_').replace(' ', '_').lower()
     mp3_path = os.path.join(plugin.config_dir, mp3_filename)
     cue_path = mp3_path + '.cue'
-    resp = requests.get(ARCHIVE_BASE_URL + url, stream=True, auth=AUTH)
-    # TODO: Assert http 200
+
     if not os.path.isfile(mp3_path):
-        plugin.log_error('{} does not exist'.format(mp3_path))
+        plugin.log_notice('{} does not exist, downloading...'.format(mp3_path))
+        resp = requests.get(ARCHIVE_BASE_URL + url, stream=True, auth=AUTH)
+        # TODO: Assert http 200
+        progress_bar = xbmcgui.DialogProgress()
+        progress_bar.create('Downloading...')
+        i = 0.0
+        file_size = int(resp.headers['Content-Length'])
         with open(mp3_path, 'wb') as f:
-            for block in resp.iter_content(4096):
+            for block in resp.iter_content(CHUNK_SIZE):
                 f.write(block)
+                i += 1
+                percent_done = int(((CHUNK_SIZE * i) / file_size) * 100)
+                progress_bar.update(percent_done, 'Please wait', '')
     if not os.path.isfile(cue_path):
         _save_cuefile(playlist, cue_path, mp3_path, params['moderators'], params['title'])
     return 'special://profile/addon_data/{}/{}'.format(PLUGIN_NAME, mp3_filename + '.cue')
